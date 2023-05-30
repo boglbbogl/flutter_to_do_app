425ec17bf53ea8757ca169632eb4b6d808378a28
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-05-30 12:50:22 +0900

    observer bloc setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_to_do/constant/observer_bloc.dart';
 import 'package:flutter_to_do/firebase_options.dart';
 
 Future<void> main() async {
@@ -7,6 +9,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  Bloc.observer = ObserverBloc();
   runApp(const TodoApp());
 }
 
