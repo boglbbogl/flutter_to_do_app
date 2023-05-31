02b78405511145691bab578ce9501c60b020b337
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-05-31 11:05:30 +0900

    go_router setting

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_to_do/constant/observer_bloc.dart';
+import 'package:flutter_to_do/constant/router.dart';
 import 'package:flutter_to_do/firebase_options.dart';
 
 Future<void> main() async {
@@ -18,6 +19,8 @@ class TodoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp.router(
+      routerConfig: router,
+    );
   }
 }
